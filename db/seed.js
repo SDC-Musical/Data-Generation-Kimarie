fdb5aedafb670215dd520ef8f416ffa59aeff21d
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-11-21 16:39:36 -0800

    Add CSV to gitignore and generate 1000 entries with new function

--- db/seed.js
+++ db/seed.js
@@ -1,39 +1,50 @@
 const faker = require('faker');
 const fs = require('fs');
 
-const generateItems = () => {
-  let productsArray = [];
+let age = ['0 to 3 years', '3 to 6 years', 'safe for ages 6 and above', '12 years and above', 'good for all ages', 'fun for all ages'];
+let playerCount = ['1 player', '2 players', '2 to 4 players', '4 players', '4 to 6 players', 'up to 10 players', '4 to 8 players', '3 to 9 players', '2 teams of 3'];
 
-  let age = ['0 to 3 years', '3 to 6 years', 'safe for ages 6 and above', '12 years and above', 'good for all ages', 'fun for all ages'];
-  let playerCount = ['1 player', '2 players', '2 to 4 players', '4 players', '4 to 6 players', 'up to 10 players', '4 to 8 players', '3 to 9 players', '2 teams of 3'];
+const writeCSVFile = fs.createWriteStream('db/products.csv');
 
-  for (let i = 0; i < 10; i++) {
-    let product = [];
-    let ageIndex = Math.floor(Math.random() * 6);
-    let playerIndex = Math.floor(Math.random() * 9);
+const streamProductsToFile = (writer, encoding, callback) => {
+  let i = 1000;
+  const write = () => {
+    let ok = true;
+    do {
+      i -= 1;
+      let product = [];
+      let csv = '';
+      let ageIndex = Math.floor(Math.random() * 6);
+      let playerIndex = Math.floor(Math.random() * 9);
 
-    let description = faker.commerce.productDescription();
-    let title = faker.commerce.productName();
-    let brand = faker.name.lastName() + ' ' + faker.lorem.words() + ' ' + faker.company.companySuffix();
-    let category = faker.commerce.productAdjective() + ' ' + faker.commerce.department();
-    let part = faker.finance.bic();
-    let GTIN = faker.finance.routingNumber();
+      let description = faker.commerce.productDescription();
+      let title = faker.commerce.productName();
+      let brand = faker.name.lastName() + ' ' + faker.lorem.words() + ' ' + faker.company.companySuffix();
+      let category = faker.commerce.productAdjective() + ' ' + faker.commerce.department();
+      let part = faker.finance.bic();
+      let GTIN = faker.finance.routingNumber();
+      
+      product.push(description, title, brand, category, age[ageIndex], playerCount[playerIndex], part, GTIN);
 
-    product.push(`"${description}"`, title, brand, category, age[ageIndex], playerCount[playerIndex], part, GTIN);
-    productsArray.push(product);
+      product.forEach((entry) => {
+        csv += entry + ';';
+      })
+
+      csv += "\n";
+      
+      if (i === 0) {
+        writer.write(csv, encoding, callback);
+      } else {
+        ok = writer.write(csv, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
   }
-  return productsArray;
-};
+write();
+}
 
-let items = generateItems();
-let csv = 'description,title,brand,catgory_name,age_category,player_count,part_Number,GTIN\n';
-
-items.forEach((item) => {
-  csv += item.join(',');
-  csv += "\n";
-})
-
-fs.writeFile('db/products.csv', csv, 'utf8', (err) => {
-  if (err) throw err;
-  console.log('CSV file successfully written!');
+streamProductsToFile(writeCSVFile, 'utf8', () => {
+  writeCSVFile.end();
 })
